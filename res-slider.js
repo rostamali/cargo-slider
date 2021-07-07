54594a41e2596195b32f017063040c4aaa9c3b39
Rostam Sardar <sardarmrostam26nov@gmail.com>
2021-07-07 18:27:18 +0600

    adding the slider code

---
+++ res-slider.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3500,
+        autoplayHoverPause:true,
+        items:1,
+    });
+});
+
+
