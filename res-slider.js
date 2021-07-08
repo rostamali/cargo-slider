72517c9aa8261976ce5718bc830e6bf2ebe421b3
Rostam Sardar <sardarmrostam26nov@gmail.com>
2021-07-08 14:13:43 +0600

    add autoplay

--- res-slider.js
+++ res-slider.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         loop:true,
-        // autoplay:true,
+        autoplay:true,
         autoplayTimeout:3500,
         autoplayHoverPause:true,
         items:1,
